279feb85574a3feddc9aa64c32d84e6dfd098dbb
Pranav Harathi <phrocks254@gmail.com>
2016-03-31 00:11:31 -0500

    Added wrapper for ISBNSearch.org

---
+++ BookMart/ISBNSearchAPI.swift
@@ -0,0 +1,27 @@
+//
+//  ISBNSearchAPI.swift
+//  BookMart
+//
+//  Created by Sita Mulomudi on 3/30/16.
+//  Copyright © 2016 CodePathFinders. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class ISBNSearchAPI {
+    
+    static let urlString = "www.isbnsearch.org/isbn/";
+    
+    // assumes valid ISBN (error handling done in CameraVC)
+    class func getInfo(ISBN: String, success: (NSURLRequest?, NSHTTPURLResponse?, NSData?) -> (), failure: (NSError?)->()) {
+        let URL = urlString + ISBN;
+        Alamofire.request(.GET, URL).response { (request, response, data, error) in
+            if let error = error {
+                failure(error)
+            } else {
+                success(request, response, data)
+            }
+        }
+    }
+}
