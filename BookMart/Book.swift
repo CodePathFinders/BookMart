23e8ff7f79ad24cbf3c4c7757e0b0a89a162b99b
navsaini <nsaini@utexas.edu>
2016-04-12 18:09:25 -0500

    Added models

---
+++ BookMart/Book.swift
@@ -0,0 +1,13 @@
+//
+//  Book.swift
+//  BookMart
+//
+//  Created by Nav Saini on 4/12/16.
+//  Copyright © 2016 CodePathFinders. All rights reserved.
+//
+
+import UIKit
+
+class Book: NSObject {
+
+}
---
+++ BookMart/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  BookMart
+//
+//  Created by Nav Saini on 4/12/16.
+//  Copyright © 2016 CodePathFinders. All rights reserved.
+//
+
+import UIKit
+
+class User: NSObject {
+
+}
